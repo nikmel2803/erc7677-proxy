9150ff39c2e53168b1a4110a8a94a6f13a2d32c4
Nikita Melnikov <nikmel2803@gmail.com>
2024-05-16 15:05:09 +0200

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
-// export const config = {
-//   runtime: 'edge'
-// }
+export const config = {
+  runtime: 'edge'
+}
 
 const app = new Hono().basePath('/api')
 
