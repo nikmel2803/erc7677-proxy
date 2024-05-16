0f7f4bb5433d2bd5c72ced8d2193071f8abe14d4
Nikita Melnikov <nikmel2803@gmail.com>
2024-05-16 15:42:24 +0200

    enable bodyParser

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 import { handle } from '@hono/node-server/vercel'
 import app from '../src/app.js'
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// }
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+}
 
 export default handle(app);
